b740ae60625ce18c0257974e547e162f6ddb872a
kaia <kaia.mattioli@gmail.com>
2018-11-09 14:28:45 -0500

    cleanup

--- analysis/08__deletions/08__deletions.py
+++ analysis/08__deletions/08__deletions.py
@@ -85,7 +85,7 @@ pool2_index_f = "%s/dels_oligo_pool.index.txt" % (index_dir)
 pool2_index_del_f = "%s/dels_oligo_pool.index.with_deletion_info.txt" % (index_dir)
 
 
-# In[12]:
+# In[6]:
 
 
 annot_f = "%s/tssid_all_biotypes.txt" % (index_dir)
@@ -125,7 +125,7 @@ index = pd.read_table(pool2_index_f, sep="\t")
 index.head()
 
 
-# In[13]:
+# In[11]:
 
 
 tfbs = pd.read_table(tfbs_f, sep="\t", header=None)
@@ -135,7 +135,7 @@ tfbs.head()
 
 # ## 2. add required deletion information to index
 
-# In[14]:
+# In[12]:
 
 
 # first create a dict of del seq : wt seq map
@@ -155,7 +155,7 @@ for i, row in index_dels.iterrows():
     dels_dict[row.element] = seq
 
 
-# In[15]:
+# In[13]:
 
 
 # add deletion number to index
@@ -163,14 +163,14 @@ print("finding deletion numbers...")
 index["del_num"] = index.apply(get_del_num, axis=1)
 
 
-# In[16]:
+# In[14]:
 
 
 print("finding deletion bases...")
 index["del_base"] = index.apply(get_del_base, seq_map=dels_dict, axis=1)
 
 
-# In[17]:
+# In[15]:
 
 
 index_elem = index.drop(["RE_count_1", "RE_count_2", "RE_count_3", "barcode", "full_oligo"], axis=1)
@@ -181,7 +181,7 @@ index_elem.head()
 
 # ## 3. merge with index
 
-# In[18]:
+# In[16]:
 
 
 hepg2_barc = index.merge(hepg2_barc_res, left_on="barcode", right_on="barcode", how="left")
@@ -189,7 +189,7 @@ k562_barc = index.merge(k562_barc_res, left_on="barcode", right_on="barcode", ho
 hepg2_barc.head()
 
 
-# In[19]:
+# In[17]:
 
 
 hepg2 = index_elem.merge(hepg2_elem_res, on=["unique_id", "element"], how="left")
@@ -197,7 +197,7 @@ k562 = index_elem.merge(k562_elem_res, on=["unique_id", "element"], how="left")
 hepg2.head()
 
 
-# In[20]:
+# In[18]:
 
 
 # add overall mean column and standard dev column
@@ -209,7 +209,7 @@ hepg2_barc["overall_mean"] = hepg2_barc[hepg2_reps].mean(axis=1)
 hepg2_barc["lfcSE"] = hepg2_barc[hepg2_reps].std(axis=1)/np.sqrt(len(hepg2_reps))
 
 
-# In[21]:
+# In[19]:
 
 
 # add overall mean column and standard dev column
@@ -223,31 +223,31 @@ k562_barc["lfcSE"] = k562_barc[k562_reps].std(axis=1)/np.sqrt(len(k562_reps))
 
 # ## 4. calculate p-value for deletions
 
-# In[22]:
+# In[20]:
 
 
 hepg2_barcode_value_dict = get_barcode_value_map(hepg2, hepg2_barc, hepg2_reps)
 
 
-# In[23]:
+# In[21]:
 
 
 k562_barcode_value_dict = get_barcode_value_map(k562, k562_barc, k562_reps)
 
 
-# In[24]:
+# In[22]:
 
 
 hepg2_pvals, hepg2_l2fcs = calculate_p_value(hepg2_barcode_value_dict)
 
 
-# In[25]:
+# In[23]:
 
 
 k562_pvals, k562_l2fcs = calculate_p_value(k562_barcode_value_dict)
 
 
-# In[26]:
+# In[24]:
 
 
 # combine and adjust pvals
@@ -255,7 +255,7 @@ hepg2_all_pvals = combine_and_adjust_pvals(hepg2_pvals, hepg2_l2fcs, 0.05, hepg2
 hepg2_all_pvals.sample(5)
 
 
-# In[27]:
+# In[25]:
 
 
 # combine and adjust pvals
@@ -263,7 +263,7 @@ k562_all_pvals = combine_and_adjust_pvals(k562_pvals, k562_l2fcs, 0.05, k562_rep
 k562_all_pvals.sample(5)
 
 
-# In[28]:
+# In[26]:
 
 
 # add info back to original dfs
@@ -271,7 +271,7 @@ hepg2 = hepg2.merge(hepg2_all_pvals, left_on="element", right_on="index", how="l
 hepg2.head()
 
 
-# In[29]:
+# In[27]:
 
 
 # add info back to original dfs
@@ -281,14 +281,14 @@ k562.head()
 
 # ## 5. wrangle deletion data into format we need
 
-# In[30]:
+# In[28]:
 
 
 unique_names = index_elem[index_elem.oligo_type.str.contains("DELETION")]["tile_name"].unique()
 [x for x in unique_names if "Enhancer" in x]
 
 
-# In[31]:
+# In[29]:
 
 
 # first flatten the dataframe so dupes at seq level are dealt with
@@ -297,7 +297,7 @@ k562 = tidy_split(k562, "dupe_info", sep=",", keep=False)
 hepg2.head()
 
 
-# In[32]:
+# In[30]:
 
 
 # then fix deletion numbers of those that have new rows due to a dupe
@@ -306,21 +306,21 @@ k562["del_num_fixed"] = k562.apply(fix_del_num, axis=1)
 hepg2.head()
 
 
-# In[33]:
+# In[31]:
 
 
 hepg2_dels = wrangle_deletion_data(hepg2, unique_names, hepg2_wt_pvals)
 hepg2_dels["Enhancer.noflip.NA__chr11:65264635-65265953__chr11:65265021..65265229,-,2.1"].head()
 
 
-# In[34]:
+# In[32]:
 
 
 k562_dels = wrangle_deletion_data(k562, unique_names, k562_wt_pvals)
 k562_dels["Enhancer.noflip.NA__chr11:65264635-65265953__chr11:65265021..65265229,-,2.1"].head()
 
 
-# In[35]:
+# In[33]:
 
 
 k562_dels["Antisense.noflip.NA__p1@MEG3__chr14:101292281..101292489,+,17.1"].head()
@@ -328,7 +328,7 @@ k562_dels["Antisense.noflip.NA__p1@MEG3__chr14:101292281..101292489,+,17.1"].hea
 
 # ## 6. write files
 
-# In[36]:
+# In[34]:
 
 
 # hepg2
@@ -348,7 +348,7 @@ name_map = pd.DataFrame({"filename": filenames, "seq_name": clean_names})
 name_map.to_csv("../../data/05__deletions/HepG2_filename_map.txt", sep="\t", index=False)
 
 
-# In[37]:
+# In[35]:
 
 
 # k562
@@ -370,7 +370,7 @@ name_map.to_csv("../../data/05__deletions/K562_filename_map.txt", sep="\t", inde
 
 # ## 7. compare deletion profiles to predicted motif profiles (seq by seq)
 
-# In[38]:
+# In[36]:
 
 
 def fix_names(row, name_dict, loc_dict):
@@ -406,7 +406,7 @@ def fix_names(row, name_dict, loc_dict):
     return clean_name
 
 
-# In[39]:
+# In[37]:
 
 
 tfbs["del_id"] = tfbs.apply(fix_names, name_dict=NAME_DICT, loc_dict=LOC_DICT, axis=1)
@@ -415,7 +415,7 @@ tfbs["del_num"] = tfbs.apply(get_del_num, axis=1)
 tfbs.sample(5)
 
 
-# In[40]:
+# In[38]:
 
 
 tfbs_wt = tfbs[(~tfbs["unique_id"].str.contains("DELETION")) & 
@@ -424,7 +424,7 @@ tfbs_wt = tfbs[(~tfbs["unique_id"].str.contains("DELETION")) &
 tfbs_wt.sample(5)
 
 
-# In[41]:
+# In[39]:
 
 
 index_del_info = index_elem[["unique_id", "dupe_info", "del_num"]]
@@ -432,21 +432,21 @@ index_tfbs = index_del_info.merge(tfbs, on=["unique_id", "del_num"])
 index_tfbs.head()
 
 
-# In[42]:
+# In[40]:
 
 
 index_tfbs = tidy_split(index_tfbs, "dupe_info", sep=",", keep=False)
 index_tfbs.head()
 
 
-# In[43]:
+# In[41]:
 
 
 index_tfbs["delpos"] = index_tfbs.apply(fix_del_num, axis=1)
 index_tfbs.head()
 
 
-# In[44]:
+# In[42]:
 
 
 hepg2_dels_fixed = {}
@@ -460,7 +460,7 @@ for dels_dict, fixed_dict in zip([hepg2_dels_clean, k562_dels_clean], [hepg2_del
 hepg2_dels_fixed["ZFAS1__p1__tile2__plus"].head()
 
 
-# In[45]:
+# In[43]:
 
 
 hepg2_all_dels_tfs = pd.DataFrame()
@@ -487,14 +487,14 @@ for dels_fixed, dels_df, cell in zip([hepg2_dels_fixed, k562_dels_fixed], [hepg2
                                 motif_vals, seq, "Fig_3C", True)
         if name == "DLEU1__p1__tile1__plus" and cell == "HepG2":
             plot_peaks_and_tfbs((5.6, 2.5), 94, name, cell, scores, yerrs, 
-                                motif_vals, seq, "Fig_S9B", True)
-        if name == "ZFAS1__p1__tile2__plus" and cell == "HepG2":
-            plot_peaks_and_tfbs((5.6, 2.5), 94, name, cell, scores, yerrs, 
-                                motif_vals, seq, "ZFAS_tile2.for_talk.pdf", True)
+                                motif_vals, seq, "Fig_S13B", True)
+#         if name == "ZFAS1__p1__tile2__plus" and cell == "HepG2":
+#             plot_peaks_and_tfbs((5.6, 2.5), 94, name, cell, scores, yerrs, 
+#                                 motif_vals, seq, "ZFAS_tile2.for_talk.pdf", True)
             
-        if name == "ZFAS1__p1__tile2__plus" and cell == "HepG2":
-            plot_peaks_and_tfbs((5.6, 1.5), 94, name, cell, scores, yerrs, 
-                                motif_vals, seq, "ZFAS_tile2.for_poster.pdf", True)
+#         if name == "ZFAS1__p1__tile2__plus" and cell == "HepG2":
+#             plot_peaks_and_tfbs((5.6, 1.5), 94, name, cell, scores, yerrs, 
+#                                 motif_vals, seq, "ZFAS_tile2.for_poster.pdf", True)
         
     if cell == "HepG2":
         hepg2_all_dels_tfs = dels_df.copy()
@@ -504,7 +504,7 @@ for dels_fixed, dels_df, cell in zip([hepg2_dels_fixed, k562_dels_fixed], [hepg2
 
 # ## 8. compare effect sizes w/ number of motifs gained or lost (for all seqs)
 
-# In[46]:
+# In[44]:
 
 
 # hepg2
@@ -516,19 +516,19 @@ sig_only = hepg2_all_dels_tfs[hepg2_all_dels_tfs["sig"] == "sig"]
 len(sig_only)
 
 
-# In[47]:
+# In[45]:
 
 
 sig_only.head()
 
 
-# In[52]:
+# In[46]:
 
 
 g.ax_joint
 
 
-# In[55]:
+# In[ ]:
 
 
 g = sns.jointplot(data=sig_only, x="delta_tfs", y="mean.log2FC", kind="reg", space=0, size=2.5, 
@@ -544,7 +544,7 @@ g.ax_joint.annotate("n = %s" % len(sig_only), ha="right", xy=(.95, .05), xycoord
 g.savefig("Fig_3B.pdf", bbox_inches="tight", dpi="figure")
 
 
-# In[56]:
+# In[ ]:
 
 
 # k562
@@ -556,7 +556,7 @@ sig_only = k562_all_dels_tfs[k562_all_dels_tfs["sig"] == "sig"]
 len(sig_only)
 
 
-# In[57]:
+# In[ ]:
 
 
 g = sns.jointplot(data=sig_only, x="delta_tfs", y="mean.log2FC", kind="reg", space=0, size=2.5, 
@@ -569,18 +569,7 @@ g.set_axis_labels(r"$\Delta$ motifs (del-ref)", "deletion effect size")
 g.ax_joint.annotate("n = %s" % len(sig_only), ha="right", xy=(.95, .05), xycoords=g.ax_joint.transAxes, 
                     fontsize=fontsize)
 
-g.savefig("Fig_S9A.pdf", bbox_inches="tight", dpi="figure")
-
-
-# In[58]:
-
-
-g = sns.jointplot(data=sig_only, x="delta_tfs", y="mean.log2FC", kind="reg", space=0, size=2.5, 
-                  marginal_kws={"hist": False}, color="darkgrey", scatter_kws={"s": 40})
-g = g.annotate(stats.spearmanr, template="spearmanr = {val:.2f}", fontsize=9)
-print("K562")
-g.set_axis_labels(r"$\Delta$ motifs (del-ref)", "deletion effect size", size=9)
-g.savefig("Fig_S9A.for_poster.pdf", bbox_inches="tight", dpi="figure")
+g.savefig("Fig_S13A.pdf", bbox_inches="tight", dpi="figure")
 
 
 # In[ ]:
